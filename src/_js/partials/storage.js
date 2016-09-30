4b81f18d11a7ad8c9b2f2735770af6f9f9e0b9b3
Andrey Gorohov <agorohov@forwardmg.ru>
2016-09-30 18:11:11 +0300

    storage

--- src/_js/partials/storage.js
+++ src/_js/partials/storage.js
@@ -1,4 +1,28 @@
 (function(window) {
   "use strict";
 
+  var Storage = function () {
+
+  }
+
+  Storage.protorype.find = function () {
+
+  }
+
+  Storage.prototype.findAll = function () {
+
+  }
+
+  Storage.prototype.save = function () {
+
+  }
+
+  Storage.prototype.delete = function () {
+
+  }
+
+  Storage.prototype.drop = function () {
+
+  }
+
 })(window);
